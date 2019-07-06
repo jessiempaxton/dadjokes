8261f292046ede0f464b9f6f9c9a0c591eed1c96
Jessie <jessiempaxton@gmail.com>
2019-07-05 18:41:52 -0600

    baseurl

--- src/store.js
+++ src/store.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Axios from 'axios'
+import router from './router'
 
 Vue.use(Vuex)
+
+let base = window.location.host.includes('localhost:8080') ? '//localhost:3000/' : '/'
+
+let api = Axios.create({
+  baseURL: 'icanhazdadjoke.com',
+  timeout: 3000,
+  // withCredentials: true
+})
 
 export default new Vuex.Store({
   state: {
